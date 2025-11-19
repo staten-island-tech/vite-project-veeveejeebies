9f767d4e76b90813228a1bb9c5da22f20b06a1c8
veeveejeebies <vavinz@nycstudents.net>
2025-11-19 12:32:21 -0500

    commit d

---
+++ viteproject.js
@@ -0,0 +1,128 @@
+const movies = [
+    
+    { 
+        name: "It",
+        category: "Horror",
+        image:
+        alt:
+    },
+     { 
+        name: "Five Night at Freddy's",
+        category: "Horror",
+        image:
+        alt:
+    },
+     { 
+        name: "Until Dawn",
+        category: "Horror",
+        image:
+        alt:
+    },
+     { 
+        name: "Weapons",
+        category: "Horror",
+        image:
+        alt:
+    },
+     { 
+        name: "Raya and the Last Dragon",
+        category: "Disney",
+        image:
+        alt:
+    },
+     { 
+        name: "Zootopia",
+        category: "Disney",
+        image:
+        alt:
+    },
+     { 
+        name: "Encanto",
+        category: "Disney",
+        image:
+        alt:
+    },
+     { 
+        name: "Wreck It Ralph", 
+        category: "Disney",
+        image:
+        alt:
+    },
+     { 
+        name: "Inside Out",
+        category: "Pixar",
+        image:
+        alt:
+    },
+     { 
+        name: "Inside Out 2", 
+        category: "Pixar",
+        image:
+        alt:
+    },
+     { 
+        name: "Turning Red",
+        category: "Pixar",  
+        image:
+        alt:
+    },
+     { 
+        name: "Luca",
+        category: "Pixar",
+        image:
+        alt:
+    },
+     { 
+        name: "Spiderman",
+        category: 
+        image:
+        alt:
+    },
+     { 
+        name: 
+        category:
+        image:
+        alt:
+    },
+     { 
+        name: 
+        category:
+        image:
+        alt:
+    },
+     { 
+        name: 
+        category:
+        price:
+        image:
+        alt:
+    },
+     { 
+        name: 
+        category:
+        price:
+        image:
+        alt:
+    },
+     { 
+        name: 
+        category:
+        price:
+        image:
+        alt:
+    },
+     { 
+        name: 
+        category:
+        price:
+        image:
+        alt:
+    },
+     { 
+        name: 
+        category:
+        price:
+        image:
+        alt:
+    },
+];
