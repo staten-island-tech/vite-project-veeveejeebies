52586531263ce7380a11c50bd37488e0cd586b06
veeveejeebies <vavinz@nycstudents.net>
2025-11-20 12:31:42 -0500

    adsdas

--- viteproject.js
+++ viteproject.js
@@ -1,103 +1,129 @@
+import "./style.css";
+
 const movies = [
-    
     { 
         name: "It",
         category: "Horror",
-        image:
-        alt:
+        image: "img/it.jpg",
+        alt: "It",
     },
-     { 
+    { 
         name: "Five Night at Freddy's",
         category: "Horror",
-        image:
-        alt:
+        image: "img/fnaf.jpg",
+        alt: "Five Night at Freddy's",
     },
-     { 
+    { 
         name: "Annabelle",
         category: "Horror",
-        image:
-        alt:
+        image: "img/Annabelle.jpg",
+        alt: "Annabelle",
     },
-     { 
+    { 
         name: "Weapons",
         category: "Horror",
-        image:
-        alt:
+        image: "img/weapons.jpg",
+        alt: "Weapons",
     },
-     { 
+    { 
         name: "Raya and the Last Dragon",
         category: "Disney",
-        image:
-        alt:
+        image: "img/raya.jpg",
+        alt: "Raya and the Last Dragon",
     },
-     { 
+    { 
         name: "Zootopia",
         category: "Disney",
-        image:
-        alt:
+        image: "img/zotopia.jpg",
+        alt: "Zootopia",
     },
-     { 
+    { 
         name: "Encanto",
         category: "Disney",
-        image:
-        alt:
+        image: "img/encanto.jpg",
+        alt: "Encanto",
     },
-     { 
+    { 
         name: "Wreck It Ralph", 
         category: "Disney",
-        image:
-        alt:
+        image: "img/ralph.jpg",
+        alt: "Wreck It Ralph",
     },
-     { 
+    { 
         name: "Inside Out",
         category: "Pixar",
-        image:
-        alt:
+        image: "img/inside 1.webp",
+        alt: "Inside Out",
     },
-     { 
+    { 
         name: "Inside Out 2", 
         category: "Pixar",
-        image:
-        alt:
+        image: "img/inside 2.jpg",
+        alt: "Inside Out 2",
     },
-     { 
+    { 
         name: "Turning Red",
         category: "Pixar",  
-        image:
-        alt:
+        image: "img/Turning Red.jpg",
+        alt: "Turning Red",
     },
-     { 
+    { 
         name: "Luca",
         category: "Pixar",
-        image:
-        alt:
+        image: "img/luca.jpg",
+        alt: "Luca",
     },
-     { 
-        name: "Spiderman",
+    { 
+        name: "Spiderman: Homecoming",
         category: "Superhero",
-        image:
-        alt:
+        image: "img/hocop.jpg",
+        alt: "Spiderman: Homecoming",
     },
-     { 
+    { 
         name: "Iron Man",
         category: "Superhero",
-        image:
-        alt:
+        image: "img/man.jpg",
+        alt: "Iron Man",
     },
-     { 
+    { 
         name: "Avengers: Infinity War",
         category: "Superhero",
-        image:
-        alt:
+        image: "img/infinity.jpg",
+        alt: "Avengers: Infinity War",
     },
-     { 
+    { 
         name: "Avengers: Endgame",
         category: "Superhero",
-        price:
-        image:
-        alt:
+        image: "img/endgame.jpg",
+        alt: "Avengers: Endgame",
     },
-  
- 
-   
 ];
+
+ const container = document.querySelector(".container");
+
+    function displayItems(list) {
+  container.innerHTML = ""; 
+  list.forEach((movie) => {
+    container.insertAdjacentHTML("beforeend", `
+<div class="body">
+ <img src="${movie.image}">
+<h1>${movie.name}</h1>
+<h2>$${movie.price}</h2>
+<button class="addCart">To Read</button>
+<button class="addCart">Already Read</button>
+</div>
+`);
+  });
+  addToCart();
+}
+
+displayItems(movies);
+
+function filter(category) {
+  const filtered = movies.filter(item => item.category === category);
+  displayItems(filtered);
+}
+
+function showAll() {
+  displayItems(movies);
+}
