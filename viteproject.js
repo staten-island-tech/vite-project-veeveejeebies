46969dfc760aa8114f3b821d2f6e908ed9967197
veeveejeebies <vavinz@nycstudents.net>
2025-11-25 12:32:19 -0500

    ddd

--- viteproject.js
+++ viteproject.js
@@ -1,108 +1,119 @@
-
-
 const movies = [
-  { name: "It",
-    category: "Horror", 
-    image: "img/it.jpg", 
-    alt: "It",
- },
-  { name: "Five Night at Freddy's",
+  { name: "It", category: "Horror", image: "img/It.jpg", alt: "It" },
+  {
+    name: "Five Night at Freddy's",
     category: "Horror",
-    image: "img/fnaf.jpg",
+    image: "img/fnaf2.jpg",
     alt: "Five Night at Freddy's",
-},
-  { name: "Annabelle", 
+  },
+  {
+    name: "Annabelle",
     category: "Horror",
-    image: "img/Annabelle.jpg", 
+    image: "img/annabelle.jpg",
     alt: "Annabelle",
-},
-  { name: "Weapons", 
-    category: "Horror", 
-    image: "img/weapons.jpg", 
-    alt: "Weapons" ,
-},
-  { name: "Raya and the Last Dragon",
-    category: "Disney", 
-    image: "img/raya.jpg", 
+  },
+  {
+    name: "Weapons",
+    category: "Horror",
+    image: "img/weapons.webp",
+    alt: "Weapons",
+  },
+  {
+    name: "Raya and the Last Dragon",
+    category: "Disney",
+    image: "img/raya.jpg",
     alt: "Raya and the Last Dragon",
- },
-  { name: "Zootopia", 
-    category: "Disney", 
-    image: "img/zotopia.jpg", 
-    alt: "Zootopia" ,
-},
-  { name: "Encanto",
-    category: "Disney", 
-    image: "img/encanto.jpg",
+  },
+  {
+    name: "Zootopia",
+    category: "Disney",
+    image: "img/zootopia.jpg",
+    alt: "Zootopia",
+  },
+  {
+    name: "Zootopia 2",
+    category: "Disney",
+    image: "img/zoo2.jpg",
     alt: "Encanto",
-},
-  { name: "Wreck It Ralph", 
+  },
+  {
+    name: "Wreck It Ralph",
     category: "Disney",
     image: "img/ralph.jpg",
-    alt: "Wreck It Ralph" ,
- },
-  { name: "Inside Out",
+    alt: "Wreck It Ralph",
+  },
+  {
+    name: "Inside Out",
     category: "Pixar",
-    image: "img/inside.webp", 
+    image: "img/inside.jpg",
     alt: "Inside Out",
-     },
-  { name: "Inside Out 2",
+  },
+  {
+    name: "Inside Out 2",
     category: "Pixar",
-    image: "img/out.jpg", 
-    alt: "Inside Out 2" ,
-    },
-  { name: "Turning Red", 
-    category: "Pixar", 
-    image: "img/meowv.jpg",
+    image: "img/inside2.webp",
+    alt: "Inside Out 2",
+  },
+  {
+    name: "Turning Red",
+    category: "Pixar",
+    image: "img/turning.jpg",
     alt: "Turning Red",
-    },
-  { name: "Luca",
-    category: "Pixar", 
-    image: "img/luca.jpg", 
-    alt: "Luca",
-},
-  { name: "Spiderman: Homecoming", 
-    category: "Superhero", 
-    image: "img/hocop.jpg",
-    alt: "Spiderman: Homecoming",
-},
-  { name: "Iron Man", 
-    category: "Superhero", 
-    image: "img/man.jpg", 
-    alt: "Iron Man",
-},
-  { name: "Avengers: Infinity War",
+  },
+  { name: "Luca", category: "Pixar", image: "img/luca.jpg", alt: "Luca" },
+  {
+    name: "Spiderman: Homecoming",
     category: "Superhero",
-    image: "img/infinity.jpg",
-    alt: "Avengers: Infinity War" 
-},
-  { name: "Avengers: Endgame", 
-    category: "Superhero", 
-    image: "img/endgame.jpg",
+    image: "img/hoco.jpg",
+    alt: "Spiderman: Homecoming",
+  },
+  {
+    name: "Iron Man",
+    category: "Superhero",
+    image: "img/ironman.jpg",
+    alt: "Iron Man",
+  },
+  {
+    name: "Avengers: Infinity War",
+    category: "Superhero",
+    image: "img/infinity.webp",
+    alt: "Avengers: Infinity War",
+  },
+  {
+    name: "Avengers: Endgame",
+    category: "Superhero",
+    image: "img/endgame.webp",
     alt: "Avengers: Endgame",
-},
+  },
 ];
 
 const container = document.querySelector(".container");
 
 function displayItems(list) {
-  container.innerHTML = ""; 
-  list.forEach(movie => {
-    container.insertAdjacentHTML("beforeend", `
-      <div class="item">
-        <img src="${movie.image}" alt="${movie.alt}">
-        <h1>${movie.name}</h1>
+  container.innerHTML = "";
+  list.forEach((movie) => {
+    container.insertAdjacentHTML(
+  "beforeend",
+  `
+  <div class="item">
+      <h1 class="title">${movie.name}</h1>
+
+      <img src="${movie.image}" alt="${movie.alt}" class="poster">
+
+      <div class="buttons">
         <button class="addCart">Add to Watchlist</button>
         <button class="addCart">Already Watched</button>
       </div>
-    `);
+  </div>
+  `
+);
   });
 }
 
 displayItems(movies);
 
 function filter(category) {
-  const filtered = movies.filter(movie => movie.category === category);
+  const filtered = movies.filter((movie) => movie.category === category);
   displayItems(filtered);
 }
 function showAll() {
